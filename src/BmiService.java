7fe94c8626ebdd7b4c1a5ff0289e67cd173fe128
Kate test <d.swetlov2017@gmail.com>
2023-09-28 09:20:58 +0300

    modified

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,10 +1,10 @@
 public class BmiService {
 
-    public double calculate(double height, double weight){
+    public double calculate(double height, double weight) {
 
         double index;
         if (weight > 0) {
-            index = weight / (height*height);
+            index = weight / (height * height);
         } else {
             index = 0;
         }
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
 
-    double height = 1.65;
+        double height = 1.65;
         double weight = 74.0;
 
-        double index = service.calculate(height,weight);
-        int indexResult= (int)index;
+        double index = service.calculate(height, weight);
+        int indexResult = (int) index;
         System.out.println(indexResult);
     }
 }
